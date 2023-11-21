1e2c613bdff48410b9960758dc13aaf717fde52e
elopezfilippini <elopezfilippini@gmail.com>
2023-11-21 20:42:52 -0300

    ss

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -10,15 +10,20 @@ export default function Cards(props)  {
      return null; // or handle the error in a way that makes sense for your application
    }
 return(
-  <div>
+  <div style={{  alignItems:"center",
+  flexDirection:"row",maxWidth:50}} className='Cartas'>
     {characters.map((character) =>(
       <div key={character.id} style={{
-         backgroundColor: 'grey',
-         fontSize: '16px',
+         backgroundColor: 'yellow', 
+         fontSize: '16  px',
          border: '3px solid red',
-         margin: '2px',
-         width : '400px'
-       }}> ;
+         margin: '2p  x',
+         width : '500px',
+     
+         
+       
+       
+       }}> <h2 style={{color:"Black"}}>Personaje </h2>
          <Card
             key={character.id}
             id={character.id}
@@ -28,6 +33,7 @@ return(
             gender={character.gender}
             origin={character.origin.name}
             image={character.image}
+            OnClose={props.OnClose} 
             />
    </div>))}
    </div>)}
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import SearchBar from './SearchBar.jsx';
+import {Link} from  "react-router-dom"
 
 export default function Nav(props) {
     const onSearch = props.onSearch
     return (
-        <SearchBar onSearch={onSearch}/>
+        <div>
+        <Link to={'/'}><button style={{ border: "1px solid #ccc"}}>Home</button></Link>
+        <Link to={'/about'}><button style={{ border: "1px solid #ccc"}} >About</button></Link>
+        <SearchBar onSearch={onSearch}/></div>
     )
 }
