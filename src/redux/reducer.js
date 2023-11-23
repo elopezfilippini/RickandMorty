a2553f28e24bee71d4d3a062b2e9c2638b6de277
elopezfilippini <elopezfilippini@gmail.com>
2023-11-23 20:06:45 -0300

    changes

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -5,29 +5,33 @@ import { filterCards } from "./actions";
 import { orderCards } from "./actions";
 import { useReducer } from "react";
 
-const initialState ={myFavorites:[]}
+const initialState ={myFavorites:[],allcharacters:[]}
 
 export const favoritos = function (state = initialState, action) {
     switch (action.type)
     {
         case addFav().type: 
         return {    
-            ...state, myFavorites:[...state.myFavorites,action.payload]            
+            ...state, myFavorites:[...state.myFavorites,action.payload],allcharacters:[...state.allcharacters,action.payload]              
         };
         
         case removeFav().type:
         return  {
-            ...state, myFavorites:state.myFavorites.filter((personajeid) => personajeid.id!==Number(action.payload))            
+            ...state, myFavorites:state.myFavorites.filter((personajeid) => personajeid.id!==Number(action.payload)), 
+            allcharacters:state.allcharacters.filter((personajeid) => personajeid.id!==Number(action.payload))            
         };
         
         case filterCards().type:
+
         return  {
-            ...state, myFavorites:state.myFavorites.filter((personajeid) => personajeid.id!==Number(action.payload))            
+            ...state, myFavorites:state.allcharacters.filter((personaje) => personaje.gender===action.payload)            
                 };
         case orderCards().type:
-        return  {
-            ...state, myFavorites:state.myFavorites.filter((personajeid) => personajeid.id!==Number(action.payload))            
-                        };
+            const ordercopy = [...myFavorites]; 
+            if(payload==="A" ) ordercopy.sort((a,b)=> a.id-b.id)
+            if(payload==="D" ) ordercopy.sort((a,b)=> b.id-a.id) 
+            return {...state,myFavorites:ordercopy} 
+       
 
 
     default:
